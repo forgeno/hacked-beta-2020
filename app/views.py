986868c8a0533ec2a9036b17672ac594934f872e
James <jamesangeloteengfo@Jamess-MBP.hitronhub.home>
2020-11-08 00:10:52 -0700

    fixed views.py, added saveExpense function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
-def homepage(request):
-    # return HttpResponse("Hello, world. You're in the expense tracker app.")
-    return render(request, 'homepage.html')
+from .models import expenseTracker
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def addExpense(request):
+    return render(request, 'addExpense.html')
+
+def saveExpense(request):
+    if request.method == 'POST':
+        try: 
+            store = int(request.POST.get('store'))
+        except ValueError:
+            return None
+        
+        try:
+            date = request.POST.get('date')
+        except AttributeError:
+            return None
+        
+        try:
+            amount = request.POST.get('amount')
+        except Exception:
+            return None
+        
+        try:
+            name = request.POST.get('name')
+        except Exception:
+            return None
+        
+        try:
+            category = request.POST.get('category')     # stored attirbutes
+        except Exception:
+            return None
+
+        createdObject = expenseTracker.objects.create(date, name, amount, store, category)  #created an object and store those attirbutes
+        createdObject.save()    # saved the object
+    
