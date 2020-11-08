b09c93f583bc00b8befd65fbebc92afffaba6153
Ivan Ma <theivanma@gmail.com>
2020-11-07 18:28:02 -0700

    Refactored repo

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -1,4 +1,4 @@
-"""expenseTracker URL Configuration
+"""djangoproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('expenseTrackerApp/', include ('expenseTrackerApp.urls')),
+    path('app/', include ('app.urls')),
     path('admin/', admin.site.urls),
 ]
--- expenseTracker/expenseTrackerApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExpensetrackerappConfig(AppConfig):
-    name = 'expenseTrackerApp'
