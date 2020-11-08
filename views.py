e2fa26c1c250dea8566b2cd2ec6f27d454d36e67
lastillo <ldm.astillo@gmail.com>
2020-11-07 21:06:54 -0700

    Added views.py

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    # return HttpResponse("Hello, world. You're in the expense tracker app.")
-    return render(request, 'homepage.html')
+    return HttpResponse("Hello, world. You're in the expense tracker app.")
+
 
 # Create your views here.
